5159529a3f5539c1b9d5abb97c7a1f23f2f27db2
zeredogo <zeredogo@gmail.com>
2022-07-27 00:58:43 +0100

    updating commit

--- src/App.js
+++ src/App.js
@@ -33,6 +33,19 @@ function App() {
       <div className='underline'></div>
     </div>
     <div className='jobs-center'>
+      <div className='btn-container'>
+        {
+          jobs.map((item, index) => {
+            return <button key={item.id} onClick={() =>
+            setValue(index)}
+            className={`job-btn ${index === value &&
+            'active-btn'}`}
+            >
+              {item.company}
+            </button>
+          })
+        }
+      </div>
       <article className='job-info'>
         <h3>{title}</h3>
         <h4>{company}</h4>
