649da30b01615e9544017c05b93c2b87b43a23af
zeredogo <zeredogo@gmail.com>
2022-07-26 23:43:24 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+
+
+const url = 'https://course-api.com/react-tabs-project'
+function App() {
+  return <h2>tabs project setup</h2>
+}
+
+export default App
